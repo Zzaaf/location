6202ea39ce18b484274f129c788be2e036170bff
Yuri Pliskovsky <yuri.pliskovsky@nevosoft.ru>
2021-03-08 15:57:07 +0300

    Version 1

---
+++ back/routes/coords.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Coords from '../models/Coords.js';
+
+const router = express.Router();
+
+router.route('/').post(async (req, res) => {
+  console.log(req.body)
+
+  const { latitude, longitude } = req.body;
+
+  if (latitude > 0 && longitude > 0) {
+    const newCoords = await new Coords({ latitude, longitude })
+    await newCoords.save()
+    res.status(201).json(newCoords)
+  } else {
+    res.json({ data: 'none' })
+  }
+});
+
+export default router;
